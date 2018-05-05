f90ab5bec1880df3d512c7386603e65a97eff320
Anton Stjernquist <antonstjernquist@gmail.com>
2018-05-05 17:57:00 +0200

    className

--- src/components/loading.js
+++ src/components/loading.js
@@ -23,10 +23,10 @@ class Loading extends Component {
         <div className="loadingDiv">
           <div>
             <h1> Loading </h1>
-            <div class="spinner">
-              <div class="bounce1"></div>
-              <div class="bounce2"></div>
-              <div class="bounce3"></div>
+            <div className="spinner">
+              <div className="bounce1"></div>
+              <div className="bounce2"></div>
+              <div className="bounce3"></div>
             </div>
           </div>
         </div>
